2b21c4c1843c23804c8ab179e9e0f1242ce81846
Reed Meher <reed.meher@gmail.com>
2023-06-17 09:57:09 -0500

    tweaking quotes formatting

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -2,42 +2,47 @@ import * as React from "react";
 
 const Quotes = () => {
     return (
-        <section className="row justify-center">
-            <div className="box-column">
-                <div className="qoute-slider">
-                    <div className="slider">
-                        <div className="mask">
-                            <ul>
-                                <li className="anim1">
-                                    <q className="qoute">Does his job with creativity, compassion, and sincere attention to detail.</q>
-                                    <p className="source">
-                                        -Coworker Brittany K.
-                                    </p>
-                                </li>
-                                <li className="anim2">
-                                    <q className="qoute">Very dependable and great at problem solving.</q>
-                                    <p className="source">
-                                        -Coworker Agne S.
-                                    </p>
-                                </li>
-                                <li className="anim3">
-                                    <q className="qoute">Reed's excellent communication skills made our efforts more of a smooth process for the whole team</q>
-                                    <p className="source">
-                                        -Alexandra D.
-                                    </p>
-                                </li>
-                                <li className="anim4">
-                                    <q className="qoute">An amazing teammate, a hard worker, an exceptional listener, and a compassionate soul.</q>
-                                    <p className="source">
-                                        -Heidi A.
-                                    </p>
-                                </li>
-                            </ul>
+        <>
+            <section className="row">
+                    <div className="qoute-slider">
+                        <div className="slider">
+                            <div className="mask">
+                                <a
+                                    href="https://www.linkedin.com/in/reed-meher/details/recommendations/?detailScreenTabIndex=0"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <ul>
+                                        <li className="anim1"><q className="qoute">Does his job with creativity, compassion, and sincere attention to detail.</q>
+                                            <p className="source">
+                                                -Coworker Brittany K.
+                                            </p>
+                                        </li>
+                                        <li className="anim2">
+                                            <q className="qoute">Very dependable and great at problem solving.</q>
+                                            <p className="source">
+                                                -Coworker Agne S.
+                                            </p>
+                                        </li>
+                                        <li className="anim3">
+                                            <q className="qoute">Reed's excellent communication skills made our efforts more of a smooth process for the whole team</q>
+                                            <p className="source">
+                                                -Alexandra D.
+                                            </p>
+                                        </li>
+                                        <li className="anim4">
+                                            <q className="qoute">An amazing teammate, a hard worker, an exceptional listener, and a compassionate soul.</q>
+                                            <p className="source">
+                                                -Heidi A.
+                                            </p>
+                                        </li>
+                                    </ul></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
+            
+            </section>
+        </>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Quotes from "../components/Quotes";
+import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <>
-       <Quotes />
+            <Quotes />
+
         </>
     )
 }
