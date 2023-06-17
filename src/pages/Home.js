3a355a57f0202d897e3929a865967599c3e849a4
Reed Meher <reed.meher@gmail.com>
2023-06-17 12:28:00 -0500

    homepage formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
 import Quotes from "../components/Quotes";
-import { Link } from "react-router-dom";
+import rm from "../images/bwPortraitRM.jpeg"
 const Home = () => {
     return (
         <>
+            <div className="container">
+                {/* <img src={rm} alt="Reed Meher" className="self-portrait" /> */}
+                <div className="container-content">
+                    <p>Reed Meher is a certified full stack web developer. He specializes in ReactJS, CSS, and learning new tricks.</p>
+                    <br />
+                    <hr />
+                    <div className="links">
+                        <a href="https://www.linkedin.com/in/reed-meher" target={'_blank'} rel={'nonreferrer'}>Follow on LinkedIn</a>
+                    </div>
+                    <br />
+                    <hr />
+                    <div className="links">
+                        <a href="https://github.com/archonology" target={'_blank'} rel={'nonreferrer'}>Follow on Github</a>
+                    </div>
+                </div>
+            </div>
             <Quotes />
 
         </>
