0827cb425e197c82578a6515c6c530fb79edb4f2
Reed Meher <reed.meher@gmail.com>
2023-11-03 14:49:04 -0500

    buidling objects to increase component implementation in resume page

---
+++ src/components/education.js
@@ -0,0 +1,37 @@
+const education = [
+    {
+        award: "Bachelor of Science, Information Science & Technology",
+        institution: "University of Wisconsin, Milwaukee",
+        dates: "01/2024 -",
+        status: "current student",
+        description: "",
+        gpa: "",
+        link: "https://uwm.edu/informationstudies/academic-programs/information-science-and-technology/",
+        certificate: "",
+        type: 'Degree'
+    },
+    {
+        award: "Full Stack Web Development Certificate",
+        institution: "University of Minnesota",
+        dates: "06/2022 - 12/2022",
+        status: "graduated",
+        description: "A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, NoSQL, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS.",
+        gpa: "",
+        link: "https://bootcamp.umn.edu/coding/landing-ftpt/?s=Google-Brand_ExternalState_&dki=Learn%20Coding%20and%20More%20Online&pkw=u%20of%20m%20coding%20bootcamp&pcrid=626983640577&pmt=e&utm_source=google&utm_medium=cpc&utm_campaign=GGL%7CUNIVERSITY-OF-MINNESOTA%7CSEM%7CCODING%7C-%7COFL%7C_ExternalState_%7CAll%7CBRD%7CEXACT%7CCore%7CBootcamp&utm_term=u%20of%20m%20coding%20bootcamp&s=google&k=u%20of%20m%20coding%20bootcamp&utm_adgroupid=145900744207&utm_locationphysicalms=9019708&utm_matchtype=e&utm_network=g&utm_device=c&utm_content=626983640577&utm_placement=&gad=1&gclid=Cj0KCQjwtJKqBhCaARIsAN_yS_m6zVV23h9zU-wM5k4ZM9sfJOtSQejHRi3SLZ4Vz7o1yNEn8LD_EnIaAjP5EALw_wcB&gclsrc=aw.ds",
+        certificate: "https://www.credly.com/badges/38a79ed6-ec04-410f-9ba1-f54002812bf5/public_url",
+        type: 'Certificate'
+    },
+    {
+        award: "Bachelor of Arts, English",
+        institution: "University of Minnesota, Morris",
+        dates: "08/2004 - 05/2004",
+        status: "graduated",
+        description: "",
+        gpa: "3.746",
+        link: "https://morris.umn.edu/",
+        certificate: "",
+        type: 'Degree'
+    }
+]
+
+export default education;
