7ee95a844a29da476c38e4e067d103c587565332
Reed Meher <reed.meher@gmail.com>
2023-12-05 11:38:17 -0600

    work card updates to font sizes

--- src/components/WorkCard/index.js
+++ src/components/WorkCard/index.js
@@ -15,17 +15,17 @@ const WorkCard = () => {
                         <Card className="edCard">
                             <CardContent>
                                 <Typography gutterBottom variant="h3"
-                                    style={{ fontSize: '16px', fontWeight: 700 }}
+                                    style={{ fontSize: '1.7vh', fontWeight: 700 }}
                                     component="div">
                                     {work.title}
                                 </Typography>
                                 <hr />
-                                <Typography style={{ fontSize: '15px', marginTop: '.5em' }}>{work.employer} | {work.location} | {work.dates}</Typography>
-                                <Typography variant="body2" color="text.secondary" style={{ paddingTop: '.7em', paddingBottom: '1em' }}>
+                                <Typography style={{ fontSize: '1.5vh', marginTop: '.5em' }}>{work.employer} | {work.location} | {work.dates}</Typography>
+                                <Typography variant="body2" style={{ paddingTop: '.7em', paddingBottom: '1em', fontSize: '1.4vh' }}>
                                     {work.summary}
                                 </Typography>
-                                <h6 style={{ fontSize: '14px' }}>Accomplishments:</h6>
-                                <ul style={{ fontSize: '14px' }}>
+                                <h6 style={{ fontSize: '1.6vh' }}>Accomplishments:</h6>
+                                <ul style={{ fontSize: '1.4vh' }}>
                                     {work.accomplishments.map((each) => {
                                         return (
                                             <li>{each}
@@ -34,15 +34,16 @@ const WorkCard = () => {
                                     })}
                                     <br />
                                 </ul>
-                                <h6 style={{ fontSize: '14px' }}>Skills:</h6>
-                                <p style={{ fontSize: '14px' }}>{work.skills}</p>
+                                <h6 style={{ fontSize: '1.6vh' }}>Skills:</h6>
+                                <p style={{ fontSize: '1.4vh' }}>{work.skills}</p>
                             </CardContent>
                             <CardActions>
                                 <Button
                                     href={work.link}
                                     target="_blank"
                                     rel="noreferrer"
-                                    size="small">Visit
+                                    
+                                    size="medium">Visit
                                 </Button>
                             </CardActions>
                         </Card>
