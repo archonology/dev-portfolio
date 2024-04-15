c0f97f02a59a61137340f39b792df808aeed7a06
Reed Meher <reed.meher@gmail.com>
2024-04-15 15:26:45 -0500

    hide resume section for now

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,33 +7,33 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 export default function Header() {
     const { theme, toggleTheme } = useAppCtx();
     const [clickState1, setClickState1] = useState('not-clicked');
-    const [clickState2, setClickState2] = useState('not-clicked');
+    // const [clickState2, setClickState2] = useState('not-clicked');
     const [clickState3, setClickState3] = useState('not-clicked');
     const [visibility, setVisibility] = useState({ visibility: 'hidden' });
 
     function handleClick1() {
         if (clickState1 === 'not-clicked') {
             setClickState1('clicked');
-            setClickState2('not-clicked');
+            // setClickState2('not-clicked');
             setClickState3('not-clicked');
         } else {
             setClickState1('not-clicked');
         }
     }
 
-    function handleClick2() {
-        if (clickState2 === 'not-clicked') {
-            setClickState2('clicked');
-            setClickState1('not-clicked');
-            setClickState3('not-clicked');
-        } else {
-            setClickState2('not-clicked');
-        }
-    }
+    // function handleClick2() {
+    //     if (clickState2 === 'not-clicked') {
+    //         setClickState2('clicked');
+    //         setClickState1('not-clicked');
+    //         setClickState3('not-clicked');
+    //     } else {
+    //         setClickState2('not-clicked');
+    //     }
+    // }
     function handleClick3() {
         if (clickState3 === 'not-clicked') {
             setClickState3('clicked');
-            setClickState2('not-clicked');
+            // setClickState2('not-clicked');
             setClickState1('not-clicked');
         } else {
             setClickState3('not-clicked');
@@ -41,7 +41,7 @@ export default function Header() {
     }
 
     function clearClicks() {
-        setClickState2('not-clicked');
+        // setClickState2('not-clicked');
         setClickState1('not-clicked');
         setClickState3('not-clicked');
     }
@@ -65,12 +65,12 @@ export default function Header() {
                             className={clickState1}>
                             <li>Portfolio</li>
                         </a>
-                        <a
+                        {/* <a
                             href='#/resume'
                             onClick={handleClick2}
                             className={clickState2}>
                             <li>Resume</li>
-                        </a>
+                        </a> */}
                         <a
                             href='#/contact'
                             onClick={handleClick3}
